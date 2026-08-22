2636e418e7f76c6d7ee896f196406540ac6d8d6c
Artyom Eroshchenko <artyom.inety@gmail.com>
2026-08-22 07:17:50 +0700

    auditing measurments (removing some from C2 and S1 vertebrae)

--- frontend/src/lib/features/medical-parameters/calculators/vertebrae.ts
+++ frontend/src/lib/features/medical-parameters/calculators/vertebrae.ts
@@ -24,91 +24,77 @@ const UP: Point = { x: 0, y: -1 };
 const RIGHT: Point = { x: 1, y: 0 };
 
 export const getVertebraeParams = (projection: Projection, v: Vertebrae, mmPerPixel: number) => {
-	if (projection == 'side') {
-		return {
-			name: v.id,
-			params: {
-				p1: { val: M.distance(v.points[1], v.points[2]) * mmPerPixel, type: 'linear' },
-				p2: { val: M.distance(v.points[0], v.points[3]) * mmPerPixel, type: 'linear' },
-				p3: { val: M.distance(v.points[0], v.points[1]) * mmPerPixel, type: 'linear' },
-				p4: { val: M.distance(v.points[2], v.points[3]) * mmPerPixel, type: 'linear' },
-				p5: {
-					name: v.id,
-					val: M.to_degrees(
-						M.get_signed_angle(
-							M.vector_sub(v.points[2], v.points[3]),
-							M.vector_sub(v.points[1], v.points[0])
-						)
-					),
-					type: 'angular'
-				},
-				p6: {
-					val: M.to_degrees(M.get_signed_angle(UP, M.vector_sub(v.points[1], v.points[0]))),
-					type: 'angular'
-				},
-				p7: {
-					val: M.to_degrees(M.get_signed_angle(UP, M.vector_sub(v.points[2], v.points[1]))),
-					type: 'angular'
-				},
-				p8: {
-					val: M.to_degrees(M.get_signed_angle(UP, M.vector_sub(v.points[3], v.points[0]))),
-					type: 'angular'
-				},
-				p9: {
-					val:
-						v.id === 'S1'
-							? M.to_degrees(M.get_signed_angle(RIGHT, M.vector_sub(v.points[2], v.points[1])))
-							: null,
-					type: 'angular'
-				}
-			}
-		};
-	} else {
-		return {
-			name: v.id,
-			params: {
-				p1: { val: M.distance(v.points[1], v.points[2]) * mmPerPixel, type: 'linear' },
-				p2: { val: M.distance(v.points[0], v.points[3]) * mmPerPixel, type: 'linear' },
-				p3: { val: M.distance(v.points[2], v.points[3]) * mmPerPixel, type: 'linear' },
-				p4: { val: M.distance(v.points[1], v.points[0]) * mmPerPixel, type: 'linear' },
-				p5: {
-					val:
-						M.distance(
-							M.get_midpoint(v.points[1], v.points[2]),
-							M.get_midpoint(v.points[0], v.points[3])
-						) * mmPerPixel,
-					type: 'linear'
-				},
-				p6: {
-					val: M.to_degrees(
-						M.get_signed_angle(
-							M.vector_sub(v.points[2], v.points[3]),
-							M.vector_sub(v.points[1], v.points[0])
-						)
-					),
-					type: 'angular'
-				},
-				p7: {
-					val: M.to_degrees(
-						M.get_signed_angle(
-							UP,
-							M.vector_sub(
-								M.get_midpoint(v.points[1], v.points[2]),
-								M.get_midpoint(v.points[0], v.points[3])
+	return {
+		name: v.id,
+		params: {
+			p1: { 
+				val: 
+					v.id != 'C2'
+						? M.distance(v.points[1], v.points[2]) * mmPerPixel
+						: null,
+				type: 'linear'
+			},
+			p2: { 
+				val: 
+					v.id != 'S1'
+						? M.distance(v.points[0], v.points[3]) * mmPerPixel
+						: null,
+				type: 'linear'
+			},
+			p3: { 
+				val: 
+					v.id != 'S1' && v.id != 'C2'
+						? M.distance(v.points[0], v.points[1]) * mmPerPixel
+						: null,
+				type: 'linear'
+			},
+			p4: { 
+				val: 
+					v.id != 'S1' && v.id != 'C2'
+						? M.distance(v.points[2], v.points[3]) * mmPerPixel
+						: null,
+				type: 'linear'
+			},
+			p5: {
+				val:
+					v.id != 'S1' && v.id != 'C2'
+						? M.to_degrees(
+							M.get_signed_angle(
+								M.vector_sub(v.points[2], v.points[3]),
+								M.vector_sub(v.points[1], v.points[0])
 							)
 						)
-					),
-					type: 'angular'
-				},
-				p8: {
-					val: M.to_degrees(M.get_signed_angle(UP, M.vector_sub(v.points[2], v.points[1]))),
-					type: 'angular'
-				},
-				p9: {
-					val: M.to_degrees(M.get_signed_angle(UP, M.vector_sub(v.points[3], v.points[0]))),
-					type: 'angular'
-				}
+						: null,
+				type: 'angular'
+			},
+			p6: {
+				val: 
+					v.id != 'S1' && v.id != 'C2'
+						? M.to_degrees(M.get_signed_angle(UP, M.vector_sub(v.points[1], v.points[0])))
+						: null,
+				type: 'angular'
+			},
+			p7: {
+				val: 
+					v.id != 'C2'
+						? M.to_degrees(M.get_signed_angle(UP, M.vector_sub(v.points[2], v.points[1])))
+						: null,
+				type: 'angular'
+			},
+			p8: {
+				val: 
+					v.id != 'S1'
+						? M.to_degrees(M.get_signed_angle(UP, M.vector_sub(v.points[3], v.points[0])))
+						: null,
+				type: 'angular'
+			},
+			p9: {
+				val:
+					v.id === 'S1'
+						? M.to_degrees(M.get_signed_angle(RIGHT, M.vector_sub(v.points[2], v.points[1])))
+						: null,
+				type: 'angular'
 			}
-		};
-	}
+		}
+	};
 };
